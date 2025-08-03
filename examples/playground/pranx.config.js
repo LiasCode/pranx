eeb573d840a9f9107267d2f675cd9f42db102fd0
LiasCode <liasraul00@gmail.com>
2025-08-02 21:00:08 -0400

    fix: use a external esbuild sass plugin

--- examples/playground/pranx.config.js
+++ examples/playground/pranx.config.js
@@ -1,11 +1,12 @@
+import sassPlugin from "esbuild-plugin-sass";
 import { tailwindPlugin } from "esbuild-plugin-tailwindcss";
 import { resolve } from "node:path";
 import { defineConfig } from "pranx";
-import { mdx_plugin, sass_plugin } from "pranx/plugins";
+import { mdx_plugin } from "pranx/plugins";
 
 export default defineConfig({
   esbuild: {
-    plugins: [mdx_plugin(), sass_plugin(), tailwindPlugin()],
+    plugins: [tailwindPlugin(), sassPlugin(), mdx_plugin()],
     alias: {
       "@ui": resolve(resolve(import.meta.dirname), "./src/components/ui/"),
       "@": resolve(resolve(import.meta.dirname), "./src/"),
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,5 +1,3 @@
-export { postcss_plugin } from "./postcss.js";
-
 export { sass_plugin } from "./sass.js";
 
 export { mdx_plugin } from "./mdx.js";
