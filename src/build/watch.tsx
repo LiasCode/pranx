6294e8f14c6f147742b90996a07c75a3d061a17d
LiasCode <liasraul00@gmail.com>
2025-08-05 04:03:29 -0400

    feat: add recalculate context

--- src/build/watch.tsx
+++ src/build/watch.tsx
@@ -1,4 +1,5 @@
 import chokidar from "chokidar";
+import type { EventName } from "chokidar/handler.js";
 import type { BuildOptions, BuildResult } from "esbuild";
 import kleur from "kleur";
 import path from "node:path";
@@ -33,22 +34,22 @@ export type OnRebuildCb = (result: ResultBundle) => Promise<void>;
 export async function watch_build(user_config: PranxConfig, onRebuild: OnRebuildCb) {
   const pages_watcher = chokidar.watch(path.join(process.cwd(), "src"));
 
-  const ctx_hydrate = await watch_bundle_hydrate({
+  let ctx_hydrate = await watch_bundle_hydrate({
     user_config,
     mode: "dev",
   });
 
-  const ctx_vendors = await watch_bundle_vendors({
+  let ctx_vendors = await watch_bundle_vendors({
     user_config,
     mode: "dev",
   });
 
-  const ctx_server = await watch_bundle_server({
+  let ctx_server = await watch_bundle_server({
     user_config,
     mode: "dev",
   });
 
-  const ctx_pages = await watch_bundle_pages({
+  let ctx_pages = await watch_bundle_pages({
     user_config,
     mode: "dev",
   });
@@ -71,10 +72,53 @@ export async function watch_build(user_config: PranxConfig, onRebuild: OnRebuild
     });
   };
 
-  pages_watcher.on("change", async (file: string) => {
-    console.log(`change: ${kleur.bold().yellow(path.relative(process.cwd(), file))} `);
-    await rebuild_cb();
+  const recalculate_context = async () => {
+    ctx_hydrate = await watch_bundle_hydrate({
+      user_config,
+      mode: "dev",
+    });
+
+    ctx_vendors = await watch_bundle_vendors({
+      user_config,
+      mode: "dev",
+    });
+
+    ctx_server = await watch_bundle_server({
+      user_config,
+      mode: "dev",
+    });
+
+    ctx_pages = await watch_bundle_pages({
+      user_config,
+      mode: "dev",
+    });
+  };
+
+  pages_watcher.on("all", async (event, file) => {
+    console.log(
+      `${get_event_name_for_user_msg(event)}: ${kleur.bold().yellow(path.relative(process.cwd(), file))}`
+    );
+
+    if (event === "change") {
+      await rebuild_cb();
+    }
+    if (event === "add" || event === "unlink") {
+      if (path.relative(user_config.pages_dir, file).startsWith(".")) {
+        return;
+      }
+      recalculate_context();
+      rebuild_cb();
+    }
   });
 
   await rebuild_cb();
 }
+
+const get_event_name_for_user_msg = (event: EventName) => {
+  if (event === "add") return "create";
+  if (event === "addDir") return "create dir";
+  if (event === "unlinkDir") return "remove dir";
+  if (event === "unlink") return "remove";
+  if (event === "change") return "update";
+  return event;
+};
