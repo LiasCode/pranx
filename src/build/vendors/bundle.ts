ac73815dc75437e5ac0a41ddf448fa1156a8d50c
LiasCode <liasraul00@gmail.com>
2025-08-19 02:09:50 -0400

    refactor: clean new base project

--- src/build/vendors/bundle.ts
+++
@@ -1,9 +0,0 @@
-import * as esbuild from "esbuild";
-import { get_vendors_config, type VendorsBundleOptions } from "./config.js";
-
-export async function bundle_vendors(options: VendorsBundleOptions) {
-  const config = await get_vendors_config(options);
-
-  await esbuild.build(config.preact);
-  await esbuild.build(config.router);
-}
---
+++ src/cmd/dev.tsx
@@ -0,0 +1,7 @@
+import { h } from "preact";
+
+export function dev() {
+  console.log("Dev Start");
+}
+
+export const Home = () => <h1>Hola Mundo</h1>;
