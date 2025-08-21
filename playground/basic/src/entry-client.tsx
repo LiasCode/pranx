82fb173f3bc4c88b884984e2a8421f929a9256c2
LiasCode <liasraul00@gmail.com>
2025-08-21 04:11:43 -0400

    refactor: remove playground basic example and update dependencies

--- playground/basic/src/entry-client.tsx
+++
@@ -1,4 +0,0 @@
-import { mount, StartApp } from "pranx";
-import "preact/devtools";
-
-mount(<StartApp />, document.body);
--- playground/basic/src/pages/about/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AboutPage() {
-  return (
-    <div>
-      <a href="/">Go to Home Page</a>
-      <h1 style={{ fontSize: "4rem" }}>About Page</h1>
-    </div>
-  );
-}
